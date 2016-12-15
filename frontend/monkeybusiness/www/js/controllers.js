127540c7d85035a054cb0591dc79228667430aef
Yerson Roa <yerson132@gmail.com>
2016-12-14 21:56:57 -0400

    Added new views to app

--- frontend/monkeybusiness/www/js/controllers.js
+++ frontend/monkeybusiness/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $state, $ionicNavBarDelegate) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -19,6 +19,13 @@ angular.module('starter.controllers', [])
     $scope.modal = modal;
   });
 
+  $ionicModal.fromTemplateUrl('templates/logup.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modalup = modal;
+  });
+
+
   // Triggered in the login modal to close it
   $scope.closeLogin = function() {
     $scope.modal.hide();
@@ -29,6 +36,14 @@ angular.module('starter.controllers', [])
     $scope.modal.show();
   };
 
+  $scope.logup = function() {
+    $scope.modalup.show();
+  };  
+
+  $scope.closeLogup = function() {
+    $scope.modalup.hide();
+  };
+
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
@@ -37,6 +52,20 @@ angular.module('starter.controllers', [])
     // code if using a login system
     $timeout(function() {
       $scope.closeLogin();
+      $state.go('app.playlists');
+      $ionicNavBarDelegate.showBackButton(false);
+    }, 1000);
+  };
+
+  $scope.doLogup = function() {
+    console.log('Doing logup', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogup();
+      $state.go('app.playlists');
+      $ionicNavBarDelegate.showBackButton(false);
     }, 1000);
   };
 })
