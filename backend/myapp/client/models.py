1c32a563be0b2e34c27a42ff4312b9d554a59212
dougtc1 <douglastorres.usb@gmail.com>
2016-12-15 02:09:43 -0400

    added unique=True to nickname, initial transaction view

--- backend/myapp/client/models.py
+++ backend/myapp/client/models.py
@@ -6,7 +6,7 @@ class Client(models.Model):
     age = models.IntegerField(verbose_name=('Edad'))
     id_number = models.CharField(max_length=200, verbose_name=('Cedula'))
     password = models.CharField(max_length=200, verbose_name=('Password'))
-    nickname = models.CharField(max_length=200, verbose_name=('Nickname'))
+    nickname = models.CharField(max_length=200, unique=True, verbose_name=('Nickname'))
     type_Client =  models.IntegerField(verbose_name=('Valor'), default=0)
     points = models.IntegerField(verbose_name=('Saldo'), default=3000)
 
